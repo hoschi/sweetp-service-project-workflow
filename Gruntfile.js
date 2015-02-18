139ae7f1a44a8002f30a52017147c4d8b8eb6e4c
Stefan Gojan <vollekannehoschi@gmail.com>
2015-02-18 17:54:12 +0100

    add forgotten CI target

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,5 +74,6 @@ module.exports = function (grunt) {
 	grunt.registerTask('codec', ['mochacov:lcov', 'codeclimate']);
 	grunt.registerTask('test', ['mochacov:unit']);
 	grunt.registerTask('default', ['jshint', 'test']);
+	grunt.registerTask('ci', ['jshint', 'test', 'codec']);
 };
 
